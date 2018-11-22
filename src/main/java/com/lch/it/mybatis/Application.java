5441ce20aac4c6abfcf1e249544bc3ca2ba22d00
liangchanghong <lchong1989@163.com>
2018-11-22 23:23:35 +0800

    qwqwqqw

--- src/main/java/com/lch/it/mybatis/Application.java
+++ src/main/java/com/lch/it/mybatis/Application.java
@@ -3,7 +3,7 @@ package com.lch.it.mybatis;
 public class Application {
 
 	public static void main(String[] args) {
-		System.err.println("2sds");
+		System.err.println("2ffffds");
 		System.err.println("112312ggg32dd23");
 	}
 }
